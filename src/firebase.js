c24b73f96d1a40ae27561f010cda26bb1e3b0f83
ultron07 <ultronreactjsdev@gmail.com>
2022-05-20 00:13:10 +0530

    update firebase config file

--- src/firebase.js
+++ src/firebase.js
@@ -14,6 +14,6 @@ const app = firebase.initializeApp({
 
 const auth = app.auth()
 const db = firebase.firestore()
-db.settings({timestampsInSnapshots:true})
+db.settings({ timestampsInSnapshots: true, merge: true });
 export {auth , db}
 export default app
